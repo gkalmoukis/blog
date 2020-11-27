4e65db39d03659af0825327256d1655c0be38d4b
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2020-11-27 16:58:02 +0200

    Add bmac button

---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+const React = require("react");
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+    setPostBodyComponents([
+        <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gkalmoukis" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can now buy me a coffee!" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script>,
+    ]);
+};
